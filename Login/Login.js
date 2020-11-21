db286da6c65aac60276e578cf7da12d16cf7c8d0
Gustavo <gustavo.rochaspj@gmail.com>
2020-11-21 10:36:56 -0300

    javascript login e atualização no html

--- Login/Login.js
+++ Login/Login.js
@@ -1,3 +1,58 @@
-function teste(){
-    alert("Testando")
+function login(){
+   let login = document.getElementById('nome').value;
+   let senha = document.getElementById('senha').value;
+   let lembrar = document.getElementById('lembrar').checked
+
+   if (!login || !senha || senha.length < 6){
+       alert("Login ou Senha inválida");
+   } else {
+    var raw = {
+        login,
+        senha,
+        lembrar
+    }
+
+    var baseURL = "https://zhang-api.herokuapp.com"
+
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "text/plain");
+    myHeaders.append("Content-Type", "application/json-patch+json");
+    myHeaders.append("origem",baseURL);
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: JSON.stringify(raw),
+        redirect: 'follow'
+    };
+
+    let statusCode = 0
+    fetch(`${baseURL}/api/Usuario/Login`, requestOptions)
+        .then(response => {
+            statusCode = response.status
+            return response.text()
+        })
+        .then(result => {
+            if (statusCode==200){
+            localStorage.setItem('usuario',result);
+            localStorage.setItem('token',JSON.parse(result).token)
+            localStorage.setItem('dadosLogin',JSON.stringify(raw))
+            window.location.href = "./../Inicial/Inicial.html"
+        } else {
+            alert(result)
+        }
+        
+        })
+        .catch(error => alert(error))
+   }
+}
+
+function iniciar(){
+    let dadosLogin = JSON.parse(localStorage.getItem('dadosLogin'))
+    if (dadosLogin && dadosLogin.lembrar){
+        console.log(dadosLogin)
+        document.getElementById('nome').value = dadosLogin.login;
+        document.getElementById('senha').value = dadosLogin.senha;
+        document.getElementById('lembrar').checked = dadosLogin.lembrar
+    }
 }
