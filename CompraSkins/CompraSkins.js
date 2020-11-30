7e853fcecef90cd17bd35195d1b7ea0bbc57354a
Gabriel Alves <gabi.12.spin@gmail.com>
2020-11-30 14:20:39 -0300

    Ajustes da compra de skin e seleção de skin

--- CompraSkins/CompraSkins.js
+++ CompraSkins/CompraSkins.js
@@ -1,12 +1,19 @@
 localStorage.setItem ("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjEiLCJuYW1laWQiOiJTdXBlclVzZXIiLCJpc2FkbSI6IjEifQ.22d5M8Zgg-Kh0CUAKVmntMuliEegGJ-MR4opvvfiQdk")
 
-const skins = [];
-const APIURL = "https://zhang-api.herokuapp.com/api/Administracao/Skins";
+const allSkins = [];
+const APIURLSkins = "https://zhang-api.herokuapp.com/api/Administracao/Skins";
+const APIURLBuyCash = "https://zhang-api.herokuapp.com/api/Jogo/CompraCash";
+const APIURLBuySkin = "https://zhang-api.herokuapp.com/api/Jogo/CompraSkin";
 const IMGPATH = "data:image/png;base64,";
 const SEARCHAPI = "https://zhang-api.herokuapp.com/api/Administracao/SkinsPorDescricao?descricaoSkin=";
-
+const user = JSON.parse(localStorage.getItem("usuario"));
 const main = document.getElementById('main');
-const skinsSpace = document.getElementById('skins-space')    
+const skinsSpace = document.getElementById('skins-space')
+const btn20 = document.getElementById('btn20');
+const btn40 = document.getElementById('btn40');
+const btn100 = document.getElementById('btn100');
+const btn300 = document.getElementById('btn300');
+let btnSkin0 = "", btnSkin1 = "", btnSkin2 = "", btnSkin3 = "";
 
 var selectedSkin;
 var drawInterval;
@@ -30,26 +37,34 @@ async function getSkins (url, token){
     showSkins(respData);
 }
 
-function showSkins (skins) {
+function showSkins (skins) {    
     var count = 0; 
     clearInterval(drawInterval);
     skinsSpace.innerHTML = '';   
     skins.forEach((skin, index) => {
         if (count < 4) {
+            allSkins.push(skin);
             const { sprite, descricao, nivel, isVip, valorCash} = skin;
             const skinEl = document.createElement('div');
             const canvasEl = document.createElement('canvas');
 
             skinEl.id = "skin" + index;
             skinEl.className = "skins";
-            skinEl.addEventListener("click", (e) => {
-                selectSkin(skinEl.id);
-            });
 
             let tagVip = "";
+            let tagBtn = "";
 
             if(isVip){
                 tagVip = '<i class="fa fa-star chequed"></i>';
+            }
+
+            if(purchasedSkin(skin.id) == true){                    
+                tagBtn = '<i class="fa fa-check"> Comprado </i>'
+            }
+            else{
+                tagBtn = `<button id="btn${skinEl.id}" class="comprar-skin d-flex justify-content-center align-items-center">
+                            Comprar
+                        </button>`
             }
 
             skinEl.classList.add('skin');
@@ -63,9 +78,7 @@ function showSkins (skins) {
                     <h4>VIP:&nbsp;</h4>
                     ${tagVip}
                 </div>
-                <button class="comprar-skin">
-                    Comprar
-                </button>
+                ${tagBtn}
             </div>
             `;
 
@@ -73,14 +86,27 @@ function showSkins (skins) {
             canvasEl.className = "canvas";
             skinEl.append(canvasEl);
             skinsSpace.appendChild(skinEl);
-            configSkin(skinEl.id, canvasEl.id)
+            skinMovement (`data:image/png;base64, ${skin.sprite}`, 0, canvasEl.id)
             count ++;
         }
     }); 
+    btnSkin0 = document.getElementById('btnskin0');
+    btnSkin1 = document.getElementById('btnskin1');
+    btnSkin2 = document.getElementById('btnskin2');
+    btnSkin3 = document.getElementById('btnskin3');
+    /*
+    if(allSkins[0] != undefined)
+        btnSkin0.onclick = comprarSkin(allSkins[0].id);
+    if(allSkins[1] != undefined)
+        btnSkin1.onclick = comprarSkin(allSkins[1].id);
+    if(allSkins[2] != undefined)
+        btnSkin2.onclick = comprarSkin(allSkins[2].id);
+    if(allSkins[3] != undefined)
+        btnSkin3.onclick = comprarSkin(allSkins[3].id);*/
 }
 
-getSkins(APIURL, localStorage.getItem("token"));
-
+getSkins(APIURLSkins, localStorage.getItem("token"));
+/*
 function configSkin (skinId, canvasId) {
     const skin = document.getElementById(skinId);
     var path, cols, rows, sheetWidth, sheetHeight;
@@ -97,7 +123,7 @@ function configSkin (skinId, canvasId) {
         cols = 5;
         sheetWidth = 864;
         sheetHeight = 346;
-        skinMovement(path, 1, cols, rows, sheetWidth, sheetHeight, canvasId);
+        skinMovement(path, 0, cols, rows, sheetWidth, sheetHeight, canvasId);
     }
     else if (skinId == "skin1") {
         path = "character.png";
@@ -114,25 +140,21 @@ function configSkin (skinId, canvasId) {
         sheetWidth = 800;
         sheetHeight = 1198;
         skinMovement(path, Math.floor(Math.random() * 2), cols, rows, sheetWidth, sheetHeight, canvasId);
-    }
-
-    
+    }    
 };
+*/
 
-function skinMovement (src, movement, cols, rows, sheetWidth, sheetHeight, canvasId) {
+function skinMovement (src, movement, canvasId) {
     var srcX;
     var srcy;
-    //var sheetWidth = 864;
-    //var sheetHeight = 280;
+    var sheetWidth = 3000;
+    var sheetHeight = 5750;
 
-    //var cols = 8;
-    //var rows = 2;
+    var width = sheetWidth / 5;
+    var height = sheetHeight / 5;
 
-    var width = sheetWidth / cols;
-    var height = sheetHeight / rows;
-
-    var maxWidth = 250;
-    var maxHeight = 150;    
+    var maxWidth = width / 2.5;
+    var maxHeight = height / 6;   
 
     var currentFrame = 0;
 
@@ -141,7 +163,6 @@ function skinMovement (src, movement, cols, rows, sheetWidth, sheetHeight, canva
     character.src = src;
 
     var canvas = document.getElementById(canvasId);
-    var canvasSize = canvas.getBoundingClientRect();
 
     canvas.style.width = maxWidth;
     canvas.style.height = maxHeight;
@@ -150,7 +171,7 @@ function skinMovement (src, movement, cols, rows, sheetWidth, sheetHeight, canva
     ctx.clearRect(0, 0, maxWidth, maxHeight);
 
     function updateFrame () {
-        currentFrame = ++currentFrame % cols;
+        currentFrame = ++currentFrame % 5;
         srcX = currentFrame * width;
         srcy = movement * height;
 
@@ -166,3 +187,82 @@ function skinMovement (src, movement, cols, rows, sheetWidth, sheetHeight, canva
         drawImage();
     }, 100);
 };
+
+function getSaldo () {
+    var saldo = document.getElementById("saldo");
+    saldo.innerHTML = '';
+    saldo.innerHTML = `<h2>SALDO:&nbsp;${user.cash}</h2>`;
+    localStorage.setItem("usuario", JSON.stringify(user));
+}
+
+getSaldo();
+
+async function buyCash(url, token, qtdCoins) {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "text/plain");
+    myHeaders.append("Content-Type", "application/json-patch+json");
+    myHeaders.append("token", token)
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow',
+    body: qtdCoins    
+    };
+    //load();
+    const resp = await fetch(url, requestOptions);
+    user.cash = await resp.json();
+    getSaldo();
+}
+
+function comprarCash(qtdeMoedas) {
+    buyCash(APIURLBuyCash, user.token, qtdeMoedas);
+    getSaldo();
+}
+
+btn20.onclick = function() {
+    comprarCash(20);
+};
+btn40.onclick = function() {
+    comprarCash(40);
+};
+btn100.onclick = function() {
+    comprarCash(100);
+};
+btn300.onclick = function() {
+    comprarCash(300);
+};
+
+function purchasedSkin (skinID) {
+    user.skins.forEach((skin, index) => {
+        if(skin.id == skinID) {
+            return true;
+        }
+        return false;
+    });
+}
+/*
+async function buySkin(url, token, skinID) {
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "text/plain");
+    myHeaders.append("Content-Type", "application/json-patch+json");
+    myHeaders.append("token", token)
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    redirect: 'follow',
+    body: skinID    
+    };
+    //load();
+    const resp = await fetch(url, requestOptions);
+    const respData = await resp.json();
+    //console.log(respData);
+    //user.skins.push(respData);
+    //localStorage.setItem("usuario", JSON.stringify(user));
+}
+
+function comprarSkin(skinID) {
+    buySkin(APIURLBuySkin, user.token, skinID);
+}
+*/
